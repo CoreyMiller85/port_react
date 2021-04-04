06132227311ef4ad5a2b44dda79e83825ac21e19
Corey Miller <coreymiller454@gmail.com>
2021-04-04 02:11:06 -0400

    adjust styling

--- src/component/Project.js
+++ src/component/Project.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PlaceHolderImg from '../img/PlaceHolderImg.jpeg'
 
 const Project = (props) => {
   return (
     <div className="project">
-      <div className="project__image">Image</div>
+      <div className="project__image"><img src={PlaceHolderImg} alt=""/></div>
       <div className="project__content">
         <h3 className="project__title">{props.title}</h3>
         <p className="project__desc">{props.desc}</p>
