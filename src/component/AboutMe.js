fd7f0b2aaeff2cfc051faaf93c69964df2d17c17
Corey Miller <coreymiller454@gmail.com>
2021-06-07 20:01:55 -0400

    update aboutme section

--- src/component/AboutMe.js
+++ src/component/AboutMe.js
@@ -1,14 +1,26 @@
 import React from "react";
+import treehouse from "../img/treehouse-seeklogo.com.svg"
+import techDegree from '../img/tech-degree-logo.png'
 
 const AboutMe = () => {
   return (
     <div className="about" id="about">
       <div className="about__card">
         <div className="about__content">
-          <h3 className="about__content__title">About Me</h3>
+          <h3 className="about__content__title">What I can do for you...</h3>
           <p className="about__content__desc">
-            Hello, I am a dude that codes things, I started out as your standard retail worker and decided I needed more in my career life. I started learning code while working full time about 5 years ago, starting with HTML, CSS and JavaScript. I fell in love with creating things from nothing and working with logic. Ever since I have been doing my best to code every day and learning something new a day. I am currently looking for a career working with code. 
+          I am a full-stack software developer with 3 years of experience, developing projects for the web. I have made splash screens to full applications with custom backends. I have completed Team TreeHouse's Tech Degree program for full-stack development. This was a 6 month long journey of learning the best practices and thought behind the current JavaScript frameworks and workflows. This program gave me strong skills in HTML, CSS, and Javascript with the ability to think through issues and come up with thoughful and maintable solutions. I have furthered this knownledge on my own through experiance and study branching into React, Redux, Mongo, and SQL. 
+            What I can bring to the table for you would be: 
+            <ul>
+              <li>A Strong willingness to learn and grow as a developer</li>
+              <li>Creating well thought out and maintainable code</li>
+              <li>A strong knowledge in React, with a deep backbone in Functional JavaScript.</li>
+              <li>Careful use of CI/CD systems.</li>
+              <li>the use of best practices</li>
+            </ul>
           </p>
+          <img src={treehouse} alt="" />
+          <img src={techDegree} alt="" />
         </div>
       </div>
     </div>
--- src/component/HireMe.js
+++ src/component/HireMe.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const HireMe = () => {
   return (
-    <div className="hire" id="contact">Hire Me Section, What can I do for you? </div>
-  )
-}
+    <div className="contact" id="contact">
+      <h3>Let me create for you</h3>
+      <div className="contact__content">
+        <div className="contact__col-1">
+          <form action="">
+            <div className="contact__form-item">
+              <label htmlFor="name">Name: </label>
+              <input type="text" name="name" />
+            </div>
+            <div className="contact__form-item">
+              <label htmlFor="name">Email: </label>
+              <input type="text" name="email" />
+            </div>
+            <div className="contact__form-item">
+              <label htmlFor="Message">Message: </label>
+              <textarea name="message" id="" cols="30" rows="10"></textarea>
+            </div>
+          </form>
+        </div>
+        <div className="contact__content__col-2">
+          <h4>You can also contact me on...</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HireMe;
