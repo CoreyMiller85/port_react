a32e2f173540fb2e95ead54e39aa4a94a7f34aba
Corey Miller <coreymiller454@gmail.com>
2021-08-01 01:51:22 -0400

    add styling on awards section

--- src/component/AboutMe.js
+++ src/component/AboutMe.js
@@ -1,30 +1,47 @@
 import React from "react";
-import treehouse from "../img/treehouse-seeklogo.com.svg"
-import techDegree from '../img/tech-degree-logo.png'
+import treehouse from "../img/treehouse-seeklogo.com.svg";
+import techDegree from "../img/tech-degree-logo.png";
 
 const AboutMe = () => {
-  return (
-    <div className="about" id="about">
-      <div className="about__card">
-        <div className="about__content">
-          <h3 className="about__content__title">What I can do for you...</h3>
-          <p className="about__content__desc">
-          I am a full-stack software developer with 3 years of experience, developing projects for the web. I have made splash screens to full applications with custom backends. I have completed Team TreeHouse's Tech Degree program for full-stack development. This was a 6 month long journey of learning the best practices and thought behind the current JavaScript frameworks and workflows. This program gave me strong skills in HTML, CSS, and Javascript with the ability to think through issues and come up with thoughful and maintable solutions. I have furthered this knownledge on my own through experiance and study branching into React, Redux, Mongo, and SQL. 
-            What I can bring to the table for you would be: 
-            <ul>
-              <li>A Strong willingness to learn and grow as a developer</li>
-              <li>Creating well thought out and maintainable code</li>
-              <li>A strong knowledge in React, with a deep backbone in Functional JavaScript.</li>
-              <li>Careful use of CI/CD systems.</li>
-              <li>the use of best practices</li>
-            </ul>
-          </p>
-          <img src={treehouse} alt="" />
-          <img src={techDegree} alt="" />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="about" id="about">
+			<div className="about__card">
+				<div className="about__content">
+					<h3 className="about__content__title">What I can do for you...</h3>
+					<p className="about__content__desc">
+						I am a full-stack software developer with 3 years of experience,
+						developing projects for the web. I have made splash screens to full
+						applications with custom backends. I have completed Team TreeHouse's
+						Tech Degree program for full-stack development. This was a 6 month
+						long journey of learning the best practices and thought behind the
+						current JavaScript frameworks and workflows. This program gave me
+						strong skills in HTML, CSS, and Javascript with the ability to think
+						through issues and come up with thoughful and maintable solutions. I
+						have furthered this knownledge on my own through experiance and
+						study branching into React, Redux, Mongo, and SQL. What I can bring
+						to the table for you would be:
+						<ul>
+							<li>A Strong willingness to learn and grow as a developer</li>
+							<li>Creating well thought out and maintainable code</li>
+							<li>
+								A strong knowledge in React, with a deep backbone in Functional
+								JavaScript.
+							</li>
+							<li>Careful use of CI/CD systems.</li>
+							<li>the use of best practices</li>
+						</ul>
+					</p>
+					<div className="about__content__awards">
+						<div className="about__content__awards-title">
+							<p>Team TreeHouse TechDegree Alumni</p>
+						</div>
+						<img src={treehouse} alt="Team treehouse logo" />
+						<img src={techDegree} alt="Treehouse techdegree logo" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default AboutMe;
--- src/component/Projects.js
+++ src/component/Projects.js
@@ -2,39 +2,27 @@ import React, { useState } from "react";
 import Project from "./Project";
 
 const Projects = () => {
-  const [projects, setProjects] = useState([
-    {
-      title: "test title 1",
-      desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, eveniet eos animi accusantium aperiam libero dignissimos consequatur facere repellat praesentium?",
-    },
-    {
-      title: "test title 2",
-      desc:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae nisi vel autem dolorum vitae blanditiis tempore quo ea modi quis.",
-    },
-    {
-      title: "test title 3",
-      desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, eveniet eos animi accusantium aperiam libero dignissimos consequatur facere repellat praesentium?",
-    },
-    {
-      title: "test title 4",
-      desc:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae nisi vel autem dolorum vitae blanditiis tempore quo ea modi quis.",
-    },
-  ]);
-  const proList = projects.map((p) => {
-    return <Project title={p.title} desc={p.desc} />;
-  });
-  return (
-    <div className="projects">
-      <div className="projects__title">
-        <h3 className="projects__title-h3">Projects</h3>
-      </div>
-      {proList}
-    </div>
-  );
+	const [projects, setProjects] = useState([
+		{
+			title: "Magic: The Gathering Collection and Card Manager",
+			desc: "This is a Full-Stack project using React, Node, MongoDB, and Express. You can view all cards from the Magic: The Gathering Game, as well as build your collection and create custom deck lists.",
+		},
+		{
+			title: "Life Counter Application",
+			desc: "A life counter compainion app for Magic: The Gathering Collectable Card Game.",
+		},
+	]);
+	const proList = projects.map((p) => {
+		return <Project title={p.title} desc={p.desc} />;
+	});
+	return (
+		<div className="projects">
+			<div className="projects__title">
+				<h3 className="projects__title-h3">Projects</h3>
+			</div>
+			{proList}
+		</div>
+	);
 };
 
 export default Projects;
